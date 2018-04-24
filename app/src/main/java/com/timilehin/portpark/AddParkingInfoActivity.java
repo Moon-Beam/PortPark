38b48477c6dfbcbda78041354b367ff5b8a0511f
olatunbosunWindows <olatunbosun.odeyinka@gmail.com>
2018-04-24 16:35:17 +0100

    Layout for AddPackingInfo done

---
+++ app/src/main/java/com/timilehin/portpark/AddParkingInfoActivity.java
@@ -0,0 +1,13 @@
+package com.timilehin.portpark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddParkingInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_parking_info);
+    }
+}
